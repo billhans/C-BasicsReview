a9eb2543cafd70dc443e287557de249786847430
H B <3343806536@qq.com>
2024-09-16 23:58:46 +0800

    variable and cout

---
+++ day02-variable/cout.cpp
@@ -0,0 +1,12 @@
+//
+// Created by flod on 2024/9/16.
+//
+#include <iostream>
+
+int main() {
+    bool truth = true;
+    std::cout << std::boolalpha;
+    std::cout << "the truth is " << truth << std::endl;
+
+    return 0;
+}
