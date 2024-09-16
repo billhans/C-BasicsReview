41db30167cacb85c8317958eae560043526ed9eb
H B <3343806536@qq.com>
2024-09-16 23:49:56 +0800

    variable

---
+++ day02-variable/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int a = 5, b = 10;
+    int sum = a + b;
+
+    std::cout << "the sum of " << a << " and " << b << " is " << sum << std::endl;
+    return 0;
+}
