3f26823ebbef89fd32a40f22731cad784f500b72
billhans <3343806536@qq.com>
2024-09-27 22:55:31 +0800

    day03

---
+++ day03-scope/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+void caculate() {
+
+}
+
+void sizeofnum() {
+
+}
+
+int globalVar = 22;
+
+int main() {
+    caculate();
+    sizeofnum();
+
+    std::cout << "Inside main: globalVar = " << globalVar << std::endl;
+
+    return 0;
+}
