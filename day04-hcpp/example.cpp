a44ed6f41a2c92e96b145206f00b94fa689372de
billhans <3343806536@qq.com>
2024-09-29 22:14:42 +0800

    day04

---
+++ day04-hcpp/example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "example.h"
+
+Myclass::Myclass() {
+
+}
+
+void Myclass::myFunction() {
+    std::cout << "hello from myclass::myfunction" << std::endl;
+}
---
+++ day04-hcpp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "example.h"
+
+int main() {
+    std::cout << "this is main cpp" << std::endl;
+    Myclass obj;
+    obj.myFunction();
+}
