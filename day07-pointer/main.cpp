15465ba41c88e26ad82925526ce1c6e1c8a097aa
H B <3343806536@qq.com>
2024-10-01 21:53:13 +0800

    day07

--- day07-pointer/main.cpp
+++ day07-pointer/main.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 int main() {
+    int var = 10;
+    int *ptr2 = &var;
+    int** ptr_address = &ptr2;
 
-    cout << "this is a example" << endl;
+    cout << "var address : " << &var << endl;
+    cout << "ptr2 value : " << ptr2 << endl;
+    cout << "ptr2 address : " << &ptr2 << endl;
+    cout << "ptr_address value : " << ptr_address << endl;
 
     return 0;
 }
