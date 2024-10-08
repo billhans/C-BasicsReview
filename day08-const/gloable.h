957f1526380ae3de2796e3a6c32924a429fdd065
billhans <3343806536@qq.com>
2024-10-08 20:39:37 +0800

    day08

---
+++ day08-const/gloable.h
@@ -0,0 +1,11 @@
+#ifndef DAY08_CONST_GLOBAL_H
+#define DAY08_CONST_GLOBAL_H
+const int bufSize = 100;
+extern const int bufSize2;
+//打印bufSize地址和bufSize2地址
+extern void PrintBufAddress();
+extern int GetSize();
+inline constexpr int GetSizeConst() {
+    return 1;
+}
+#endif //DAY08_CONST_GLOBAL_H
---
+++ day08-const/global.cpp
@@ -0,0 +1,15 @@
+
+#include "global.h"
+#include <iostream>
+
+const int bufSize2 = 10;
+void PrintBufAddress(){
+    std::cout << "global.cpp buf address: " << &bufSize << std::endl;
+    std::cout << "global.cpp buf2 address: " << &bufSize2 << std::endl;
+}
+
+int GetSize(){
+    return 20;
+}
+
+
